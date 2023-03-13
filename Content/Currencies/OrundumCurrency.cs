0dd754cd2e103b01876da4d92d3ff91e738ef283
cueVs <118194388+cueVs@users.noreply.github.com>
2023-03-13 17:50:50 +0900

    modified some points for new version

--- Content/Currencies/OrundumCurrency.cs
+++ Content/Currencies/OrundumCurrency.cs
@@ -7,8 +7,7 @@ namespace ArknightsMod.Content.Currencies
 {
 	public class OrundumCurrency : CustomCurrencySingleCoin
 	{
-		public OrundumCurrency(int coinItemID, long currencyCap, string CurrencyTextKey) : base(coinItemID, currencyCap)
-		{
+		public OrundumCurrency(int coinItemID, long currencyCap, string CurrencyTextKey) : base(coinItemID, currencyCap) {
 			this.CurrencyTextKey = CurrencyTextKey;
 		}
 	}
