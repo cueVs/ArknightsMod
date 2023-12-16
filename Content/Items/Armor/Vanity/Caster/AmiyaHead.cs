0adc156b6bc1fe23d06d5706e57cb8ba8b7afd75
cueV <118194388+cueVs@users.noreply.github.com>
2023-12-16 17:00:07 +0900

    add Amiya's outfit

---
+++ Content/Items/Armor/Vanity/Caster/AmiyaHead.cs
@@ -0,0 +1,32 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace ArknightsMod.Content.Items.Armor.Vanity.Caster
+{
+	// See also: ExampleCostume
+	[AutoloadEquip(EquipType.Head)]
+	public class ChenHead : ModItem
+	{
+		public override void SetStaticDefaults() {
+			// DisplayName.SetDefault("Arknights Doctor's Hood");
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults() {
+			Item.width = 20;
+			Item.height = 12;
+			Item.rare = ItemRarityID.Pink;
+			Item.vanity = true;
+		}
+
+		//public override void AddRecipes()
+		//{
+		//    Recipe recipe = CreateRecipe();
+		//    recipe.AddRecipeGroup(RecipeGroupID.Wood, 2);
+		//    recipe.AddTile(TileID.WorkBenches);
+		//    recipe.Register();
+		//}
+	}
+}
