b575065689bfff7091452702d23a5e4e3ce67806
RegRoLLL <156439463+RegRoLLL@users.noreply.github.com>
2024-01-20 18:07:34 +0900

    Add StunDebuff script file
    ・added StunDebuff.cs
    ・added StunDebuff.png (temporary)

---
+++ Content/Buffs/StunDebuff.cs
@@ -0,0 +1,36 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using System.Collections.Generic;
+using System.Security.Principal;
+
+namespace ArknightsMod.Content.Buffs
+{
+	public class StunDebuff : ModBuff
+	{
+		public override void SetStaticDefaults() {
+			Main.pvpBuff[Type] = true;
+			Main.buffNoSave[Type] = true;
+			Main.debuff[Type] = true;
+		}
+
+		public override void Update(Player player, ref int buffIndex) {
+			//stun parameters
+		}
+
+		public override void Update(NPC npc, ref int buffIndex) {
+			//stun parameters
+		}
+	}
+
+
+	public class StunDebuffPlayer : ModPlayer
+	{
+		
+	}
+
+	public class StunDebuffGlobalNPC : GlobalNPC
+	{
+
+	}
+}
