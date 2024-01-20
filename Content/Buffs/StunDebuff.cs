e0d7016f649d6a0a69ef76c341eafc785ee15f7e
RegRoLLL <156439463+RegRoLLL@users.noreply.github.com>
2024-01-21 08:39:59 +0900

    added stun effect(temporary)
    added temporary stun effect (blackout)

--- Content/Buffs/StunDebuff.cs
+++ Content/Buffs/StunDebuff.cs
@@ -3,6 +3,8 @@ using Terraria.ModLoader;
 using Terraria.ID;
 using System.Collections.Generic;
 using System.Security.Principal;
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
 
 namespace ArknightsMod.Content.Buffs
 {
@@ -15,10 +17,14 @@ namespace ArknightsMod.Content.Buffs
 		}
 
 		public override void Update(Player player, ref int buffIndex) {
+			player.GetModPlayer<StunDebuffPlayer>().stunDebuff = true;
+
 			//stun parameters
 		}
 
 		public override void Update(NPC npc, ref int buffIndex) {
+			npc.GetGlobalNPC<StunDebuffGlobalNPC>().stunDebuff = true;
+
 			//stun parameters
 		}
 	}
@@ -26,11 +32,40 @@ namespace ArknightsMod.Content.Buffs
 
 	public class StunDebuffPlayer : ModPlayer
 	{
-		
+		public bool stunDebuff;
+
+		public override void ResetEffects() {
+			stunDebuff = false;
+		}
+
+		public override void DrawEffects(
+			PlayerDrawSet drawInfo,
+			ref float r, ref float g, ref float b, ref float a,
+			ref bool fullBright
+			)
+			{
+			r *= 0.5f;
+			g *= 0.5f;
+			b *= 0.5f;
+		}
 	}
 
 	public class StunDebuffGlobalNPC : GlobalNPC
 	{
+		public bool stunDebuff;
 
+		public override bool InstancePerEntity => true;
+
+		public override void ResetEffects(NPC npc) {
+			stunDebuff = false;
+		}
+
+		public override void DrawEffects(NPC npc, ref Color drawColor) {
+			if (stunDebuff) {
+				drawColor.R *= (byte)0.5;
+				drawColor.G *= (byte)0.5;
+				drawColor.B *= (byte)0.5;
+			}
+		}
 	}
 }
