a8f738f8c0576200926c264baf60734d2f78cb0d
cueV <118194388+cueVs@users.noreply.github.com>
2024-01-27 18:27:56 +0900

    Add a costume of Matoimaru

---
+++ Content/Items/Armor/Vanity/Guard/MatoimaruLegs.cs
@@ -0,0 +1,36 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace ArknightsMod.Content.Items.Armor.Vanity.Guard
+{
+	// See also: ExampleCostume
+	[AutoloadEquip(EquipType.Legs)]
+	public class MatoimaruLegs : ModItem
+	{
+		public override void SetStaticDefaults() {
+			// DisplayName.SetDefault("Arknights Doctor's Pants");
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+
+			if (Main.netMode == NetmodeID.Server)
+				return;
+			ArmorIDs.Legs.Sets.HidesBottomSkin[Item.legSlot] = true;
+		}
+
+		public override void SetDefaults() {
+			Item.width = 16;
+			Item.height = 6;
+			Item.rare = ItemRarityID.LightPurple; // Same rarity as Arknights
+			Item.vanity = true;
+		}
+
+		//public override void AddRecipes()
+		//{
+		//    Recipe recipe = CreateRecipe();
+		//    recipe.AddRecipeGroup(RecipeGroupID.Wood, 2);
+		//    recipe.AddTile(TileID.WorkBenches);
+		//    recipe.Register();
+		//}
+	}
+}
