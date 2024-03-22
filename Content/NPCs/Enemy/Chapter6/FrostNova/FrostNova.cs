d77e50cc91ef9da1998b16d868c9523ab8467cff
cueV <118194388+cueVs@users.noreply.github.com>
2024-03-22 18:33:46 +0900

    add FrostNova's revival state

--- Content/NPCs/Enemy/Chapter6/FrostNova/FrostNova.cs
+++ Content/NPCs/Enemy/Chapter6/FrostNova/FrostNova.cs
@@ -35,12 +35,14 @@ namespace ArknightsMod.Content.NPCs.Enemy.Chapter6.FrostNova
 
 		private enum ActionState : int
 		{
+			Jump,
 			Walk,
+			FastWalk,
 			SmallAttack,
 			LargeAttack,
-			Dying,
+			Sing,
 			Revival,
-			Sing
+			Dying
 		};
 
 		private ActionState State {
@@ -57,7 +59,12 @@ namespace ArknightsMod.Content.NPCs.Enemy.Chapter6.FrostNova
 			}
 		}
 
-		public ref float Death_Timer => ref NPC.localAI[0];
+		public ref float DeathTimer => ref NPC.localAI[0];
+		public ref float JumpTimer => ref NPC.localAI[1];
+
+		public ref float StateTimer => ref NPC.localAI[1];
+		public override float SpawnChance(NPCSpawnInfo spawnInfo) => spawnInfo.Player.ZoneOverworldHeight && !NPC.AnyNPCs(NPCType<FrostNova>()) ? 0f : 0f;
+
 
 		// Do NOT try to use NPC.ai[4]/NPC.localAI[4] or higher indexes, it only accepts 0, 1, 2 and 3!
 		// If you choose to go the route of "wrapping properties" for NPC.ai[], make sure they don't overlap (two properties using the same variable in different ways), and that you don't accidently use NPC.ai[] directly
@@ -88,8 +95,8 @@ namespace ArknightsMod.Content.NPCs.Enemy.Chapter6.FrostNova
 		}
 
 		public override void SetDefaults() {
-			NPC.width = 60;
-			NPC.height = 62;
+			NPC.width = 40;
+			NPC.height = 58;
 			NPC.damage = 12;
 			NPC.defense = 10;
 			NPC.lifeMax = 200;
@@ -118,6 +125,8 @@ namespace ArknightsMod.Content.NPCs.Enemy.Chapter6.FrostNova
 			if (!Main.dedServ) {
 				Music = MusicLoader.GetMusicSlot(Mod, "Music/Lullabye");
 			}
+			State = ActionState.Walk;
+			JumpTimer = 0;
 		}
 
 		public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry) {
@@ -163,7 +172,7 @@ namespace ArknightsMod.Content.NPCs.Enemy.Chapter6.FrostNova
 			if (State != ActionState.Dying && SecondStage) //If the boss is defeated, but the death animation hasn't played yet, play the death animation.
 			{
 				State = ActionState.Dying; //Flag boss as "dying"
-				Death_Timer = 0;
+				DeathTimer = 0;
 				NPC.damage = 0; //Disable contact damage
 				NPC.life = 1;
 				NPC.dontTakeDamage = true; //Invulnerable
@@ -171,7 +180,7 @@ namespace ArknightsMod.Content.NPCs.Enemy.Chapter6.FrostNova
 				return false; //Boss isn't dead yet!
 			}
 			if (State != ActionState.Revival && !SecondStage) {
-				Death_Timer = 0;
+				DeathTimer = 0;
 				NPC.life = 1;
 				State = ActionState.Revival;
 				SecondStage = true;
@@ -184,6 +193,7 @@ namespace ArknightsMod.Content.NPCs.Enemy.Chapter6.FrostNova
 
 		private void SwitchTo(ActionState state) {
 			State = state;
+			NPC.netUpdate = true;
 		}
 
 		public override void FindFrame(int frameHeight) {
@@ -194,11 +204,14 @@ namespace ArknightsMod.Content.NPCs.Enemy.Chapter6.FrostNova
 			int frameSpeed;
 			NPC.spriteDirection = NPC.direction;
 
-			if (State == ActionState.Walk) {
+			if (State == ActionState.Walk || State == ActionState.FastWalk || State == ActionState.Jump) {
 				startFrame = 0;
 				finalFrame = 4;
 
 				frameSpeed = 5;
+				if (State == ActionState.FastWalk) {
+					frameSpeed = 4;
+				}
 				NPC.frameCounter += 0.5f;
 				if (NPC.frameCounter > frameSpeed) {
 					NPC.frameCounter = 0;
@@ -281,6 +294,7 @@ namespace ArknightsMod.Content.NPCs.Enemy.Chapter6.FrostNova
 			}
 
 			if (State == ActionState.Dying) {
+				NPC.velocity.X = 0f;
 				Dying();
 				return;
 			}
@@ -297,33 +311,23 @@ namespace ArknightsMod.Content.NPCs.Enemy.Chapter6.FrostNova
 		}
 
 
-
-		//private void CheckSecondStage() {
-		//	if (SecondStage) {
-		//		// No point checking if the NPC is already in its second stage
-		//		return;
-		//	}
-
-		//	if (NPC.life <= 0 && Main.netMode != NetmodeID.MultiplayerClient) {
-		//		// If we have no shields (aka "no minions alive"), we initiate the second stage, and notify other players that this NPC has reached its second stage
-		//		// by setting NPC.netUpdate to true in this tick. It will send important data like position, velocity and the NPC.ai[] array to all connected clients
-
-		//		State = ActionState.Revival;
-		//		NPC.life = NPC.lifeMax; //HP set to max
-		//		// Because SecondStage is a property using NPC.ai[], it will get synced this way
-		//		SecondStage = true;
-		//		NPC.netUpdate = true;
-		//	}
-		//}
-
 		private void DoFirstStage(Player player) {
-			State = ActionState.Walk;
+			if (State == ActionState.Walk) {
+				Walk();
+			}
+			if (State == ActionState.FastWalk) {
+				FastWalk();
+			}
+			if (State == ActionState.Jump) {
+				Jump();
+			}
 		}
 
 		private void DoSecondStage(Player player) {
 			if (State == ActionState.Revival) {
-				Death_Timer++;
-				if (Death_Timer > 2 * 60) {
+				NPC.velocity.X = 0f;
+				DeathTimer++;
+				if (DeathTimer > 2 * 60) {
 					NPC.life++;
 					if (NPC.life > NPC.lifeMax) {
 						NPC.life = NPC.lifeMax;
@@ -331,16 +335,24 @@ namespace ArknightsMod.Content.NPCs.Enemy.Chapter6.FrostNova
 				}
 				return;
 			}
-			else if (State == ActionState.Walk){
-				if (Death_Timer > 0) {
+			else if (State == ActionState.Walk) {
+				Walk();
+
+				if (DeathTimer > 0) {
 					NPC.life = NPC.lifeMax;
 					NPC.dontTakeDamage = true;
-					Death_Timer++;
-					if (Death_Timer > 25 * 60) {
+					DeathTimer++;
+					if (DeathTimer > 10 * 60) { // TODO
 						NPC.dontTakeDamage = false;
-						Death_Timer = 0;
+						DeathTimer = 0;
 					}
 				}
+			}
+			else if (State == ActionState.FastWalk) {
+				FastWalk();
+			}
+			else if (State == ActionState.Jump) {
+				Jump();
 			}
 
 
@@ -348,9 +360,9 @@ namespace ArknightsMod.Content.NPCs.Enemy.Chapter6.FrostNova
 
 
 		private void Dying() {
-			Death_Timer++;
+			DeathTimer++;
 
-			if (Death_Timer == 130f) {
+			if (DeathTimer == 130f) {
 				if (Main.netMode != NetmodeID.MultiplayerClient) {
 					//Vector2 po = NPC.Center + new Vector2(Main.rand.Next(-60, 0), Main.rand.Next(-30, 10));
 					//float positionX = NPC.Center.X - 10;
@@ -359,36 +371,135 @@ namespace ArknightsMod.Content.NPCs.Enemy.Chapter6.FrostNova
 						float positionX = NPC.Center.X + Main.rand.NextFloat(-20, 23) * NPC.direction;
 						float positionY = NPC.Center.Y + Main.rand.NextFloat(0, 45);
 						Vector2 position = new Vector2(positionX, positionY);
-						Projectile.NewProjectile(null, position, new Vector2(-0.5f, -0.3f), ProjectileType<FrostNovaSmoke>(), 0, 0f, -1, 0, 90, 0);
+						Projectile.NewProjectile(null, position, new Vector2(0.5f * NPC.direction, -0.3f), ProjectileType<FrostNovaSmoke>(), 0, 0f, -1, 0, 90, 0);
 					}
 					for (int i = 0; i < 4; i++) {
 						float positionX = NPC.Center.X + Main.rand.NextFloat(-34, 16) * NPC.direction;
 						float positionY = NPC.Center.Y + Main.rand.NextFloat(-10, 34);
 						Vector2 position = new Vector2(positionX, positionY);
-						Projectile.NewProjectile(null, position, new Vector2(-2.8f, -0.5f), ProjectileType<FrostNovaSmoke>(), 0, 0f, -1, 0, 80, -1);
+						Projectile.NewProjectile(null, position, new Vector2(2.8f * NPC.direction, -0.5f), ProjectileType<FrostNovaSmoke>(), 0, 0f, -1, 0, 80, -1);
 					}
 					for (int i = 0; i < 4; i++) {
 						float positionX = NPC.Center.X + Main.rand.NextFloat(-18, 28) * NPC.direction;
 						float positionY = NPC.Center.Y + Main.rand.NextFloat(-29, 16);
 						Vector2 position = new Vector2(positionX, positionY);
-						Projectile.NewProjectile(null, position, new Vector2(3f, 0.7f), ProjectileType<FrostNovaSmoke>(), 0, 0f, -1, 0, 85, 1);
+						Projectile.NewProjectile(null, position, new Vector2(-3f * NPC.direction, 0.7f), ProjectileType<FrostNovaSmoke>(), 0, 0f, -1, 0, 85, 1);
 					}
 					for (int i = 0; i < 3; i++) {
 						float positionX = NPC.Center.X + Main.rand.NextFloat(-10, 33) * NPC.direction;
 						float positionY = NPC.Center.Y + Main.rand.NextFloat(10, 36);
 						Vector2 position = new Vector2(positionX, positionY);
-						Projectile.NewProjectile(null, position, new Vector2(0.2f, -0.4f), ProjectileType<FrostNovaSmoke>(), 0, 0f, -1, 0, 85, 1);
+						Projectile.NewProjectile(null, position, new Vector2(-0.9f * NPC.direction, -0.4f), ProjectileType<FrostNovaSmoke>(), 0, 0f, -1, 0, 85, 1);
 					}
 				}
 			}
 
-			if (Death_Timer >= 135f) {
+			if (DeathTimer >= 135f) {
 				NPC.life = 0;
 				NPC.HitEffect(0, 0);
 				NPC.checkDead(); // This will trigger ModNPC.CheckDead the second time, causing the real death.
 			}
 
 			return;
+		}
+		private void Walk() {
+			int direction = (Main.player[NPC.target].Center.X > NPC.Center.X).ToDirectionInt();
+			StateTimer++;
+			if (NPC.collideX) {
+				JumpTimer++;
+			}
+			else {
+				JumpTimer = 0;
+			}
+
+			if (JumpTimer > 0 && JumpTimer < 30) {
+				NPC.direction = direction;
+				NPC.velocity.X = 0.2f * direction;
+				NPC.velocity.Y = -1f;
+			}
+			else if (JumpTimer >= 30) {
+				SwitchTo(ActionState.Jump);
+				JumpTimer = 0;
+			}
+			else {
+				NPC.direction = direction;
+				NPC.velocity.X = 0.2f * direction;
+				if (Main.rand.NextBool(100) && DeathTimer <= 0 && StateTimer > 2 * 60) {
+					StateTimer = 0;
+					State = ActionState.FastWalk;
+					NPC.netUpdate = true;
+					FastWalk();
+				}
+			}
+		}
+
+		private void FastWalk() {
+			int direction = (Main.player[NPC.target].Center.X > NPC.Center.X).ToDirectionInt();
+			StateTimer++;
+			if (NPC.collideX) {
+				JumpTimer++;
+			}
+			else {
+				JumpTimer = 0;
+			}
+
+			if (JumpTimer > 0 && JumpTimer < 30) {
+				NPC.direction = direction;
+				NPC.velocity.X = 0.3f * direction;
+				NPC.velocity.Y = -1f;
+			}
+			else if (JumpTimer >= 30) {
+				SwitchTo(ActionState.Jump);
+				JumpTimer = 0;
+			}
+			else {
+				NPC.direction = direction;
+				NPC.velocity.X = 0.4f * direction;
+				if (Main.rand.NextBool(100) && StateTimer > 2 * 60) {
+					StateTimer = 0;
+					State = ActionState.Walk;
+					NPC.netUpdate = true;
+					Walk();
+				}
+			}
+		}
+		private void Jump() {
+			int direction = (Main.player[NPC.target].Center.X > NPC.Center.X).ToDirectionInt();
+			JumpTimer++;
+			if (JumpTimer < 120) {
+				NPC.direction = -direction;
+				NPC.velocity.X = -0.2f * direction;
+			}
+			else if (JumpTimer >= 120 && JumpTimer < 140) {
+				NPC.direction = direction;
+				NPC.velocity.X = 0.5f * direction;
+				NPC.velocity.Y = Math.Min(-6f + (JumpTimer - 120f) * 0.1f, - 0.2f);
+			}
+			else if (!NPC.collideY && JumpTimer < 280) {
+				NPC.direction = direction;
+				NPC.velocity.X = 0.8f * direction;
+				NPC.velocity.Y = 0.5f;
+			}
+			else {
+				SwitchTo(ActionState.Walk);
+				JumpTimer = 0;
+			}
+
+			if (JumpTimer == 119) {
+				for (int i = 0; i < 3; i++) {
+					float positionX = NPC.Center.X + Main.rand.NextFloat(-15, 15) * NPC.direction;
+					float positionY = NPC.Center.Y + Main.rand.NextFloat(0, 5);
+					Vector2 position = new Vector2(positionX, positionY);
+					Projectile.NewProjectile(null, position, new Vector2(1f * NPC.direction, 1f), ProjectileType<FrostNovaJump>(), 0, 0f, -1, 0, 40, 0);
+				}
+				for (int i = 0; i < 3; i++) {
+					float positionX = NPC.Center.X + Main.rand.NextFloat(-15, 15) * NPC.direction;
+					float positionY = NPC.Center.Y + Main.rand.NextFloat(0, 5);
+					Vector2 position = new Vector2(positionX, positionY);
+					Projectile.NewProjectile(null, position, new Vector2(-1f * NPC.direction, 1f), ProjectileType<FrostNovaJump>(), 0, 0f, -1, 0, 40, 0);
+				}
+				SoundEngine.PlaySound(SoundID.DoubleJump, NPC.Center);
+			}
 		}
 	}
 }
---
+++ Content/Projectiles/Bosses/FrostNova/FrostNovaJump.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
+using ReLogic.Content;
+using System;
+using static Terraria.ModLoader.ModContent;
+using Terraria.ID;
+using ArknightsMod.Content.Items.Material;
+
+namespace ArknightsMod.Content.Projectiles.Bosses.FrostNova
+{
+	// Shortsword projectiles are handled in a special way with how they draw and damage things
+	// The "hitbox" itself is closer to the player, the sprite is centered on it
+	// However the interactions with the world will occur offset from this hitbox, closer to the sword's tip (CutTiles, Colliding)
+	// Values chosen mostly correspond to Iron Shortword
+	public class FrostNovaJump : ModProjectile
+	{
+		public override void SetDefaults() {
+			Projectile.width = 80;
+			Projectile.height = 80;
+			Projectile.aiStyle = 0;
+			Projectile.timeLeft = 250;
+			Projectile.penetrate = -1;
+			Projectile.scale = 0.01f;
+			Projectile.Opacity = 0f;
+			Projectile.hide = false;
+			Projectile.hostile = false;
+			Projectile.friendly = false;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = false;
+			Projectile.netUpdate = true;
+		}
+
+		// Allows you to determine the color and transparency in which a projectile is drawn
+		// Return null to use the default color (normally light and buff color)
+		// Returns null by default.
+		//public override Color? GetAlpha(Color lightColor)
+		//{
+		//    return new Color(0, 50, 100, 0) * Projectile.Opacity;
+		//}
+
+		public override void AI() {
+			Projectile.ai[0] += 1;
+			if (Projectile.localAI[0] == 0f) {
+				Projectile.rotation = Main.rand.NextFloat(MathHelper.TwoPi);
+				Projectile.localAI[0] = Main.rand.Next(2) * 2 -1; // 1 or -1
+				Projectile.localAI[1] = Main.rand.NextFloat(Projectile.ai[1], Projectile.ai[1] + 40);
+				Projectile.localAI[2] = Main.rand.Next(1, 5);
+			}
+			//NPC npc = Main.npc[Projectile.owner];
+			//float positionX = npc.position.X - 10;
+			//float positionY = npc.position.Y;
+			//if (Projectile.ai[1] > 0f) {
+			//	Projectile.velocity.X = Math.Max(Projectile.velocity.X - 0.1f, 0f);
+			//}
+			//else {
+			//	Projectile.velocity.X = Math.Min(Projectile.velocity.X + 0.1f, 0f);
+			//}
+			//if (Projectile.ai[2] > 0f) {
+			//	Projectile.velocity.Y = Math.Max(Projectile.velocity.Y - 0.1f, 0f);
+			//}
+			//else {
+			//	Projectile.velocity.Y = Math.Min(Projectile.velocity.Y + 0.1f, 0f);
+			//}
+
+			//Projectile.velocity.X = GetSPV(Projectile.ai[1], Projectile.ai[1] + 20, Projectile.velocity.X, 2);
+			//Projectile.velocity.Y = GetSPV(Projectile.ai[2], Projectile.ai[2] + 20, Projectile.velocity.Y, 2);
+
+			if (Projectile.ai[0] >= 250)
+				Projectile.Kill();
+
+			FadeInAndOut();
+		}
+
+		// Many projectiles fade in so that when they spawn they don't overlap the gun muzzle they appear from
+		public void FadeInAndOut() {
+			// If last less than 50 ticks — fade in, than more — fade out
+			if (Projectile.ai[0] <= Projectile.localAI[1]) {
+				// Fade in
+				Projectile.Opacity += 0.1f;
+				Projectile.scale += 0.05f;
+				Projectile.rotation += 0.003f * Projectile.ai[2];
+				Projectile.velocity *= 0.95f;
+				// Cap
+				if (Projectile.Opacity > 1f)
+					Projectile.Opacity = 1f;
+				if (Projectile.scale > 1f) {
+					Projectile.scale = 1f;
+				}
+				return;
+			}
+
+			// Fade out
+			Projectile.Opacity -= 0.03f;
+			Projectile.velocity *= 0;
+			Projectile.rotation += 0.005f * Projectile.ai[2];
+			if (Projectile.Opacity < 0)
+				Projectile.Opacity = 0;
+			//if (Projectile.localAI[1] <= Projectile.ai[0] && Projectile.ai[0] <= (Projectile.localAI[1] + 60f) && Projectile.ai[0] % 10 == 0 && Projectile.localAI[0] == 1) {
+			//	Dust dust = Main.dust[Dust.NewDust(Projectile.Left + new Vector2(-30, -25), 50, Projectile.height / 2, DustType<Dusts.Bosses.FrostNovaDeathDust>(), 0f, -2f)];
+			//	dust.noGravity = true;
+			//	dust.fadeIn = 0f;
+			//	dust.scale = 1.5f;
+			//}
+		}
+
+		public override bool PreDraw(ref Color lightColor) {
+			Main.spriteBatch.End();
+			Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive, Main.DefaultSamplerState, DepthStencilState.None, Main.Rasterizer, null, Main.GameViewMatrix.TransformationMatrix);
+
+
+			Texture2D texture = Request<Texture2D>("ArknightsMod/Assets/GrayScaleTexture/Smoke" + (int)Projectile.localAI[2], AssetRequestMode.ImmediateLoad).Value;
+			float opacity = Projectile.Opacity * 0.6f;
+			Color color = Color.White * opacity;
+			float scale = Projectile.scale;
+			Vector2 origin = texture.Size() * 0.5f;
+			//float rotation = 2f * (float)Math.PI * Main.rand.NextFloat();
+			Main.spriteBatch.Draw(texture, Projectile.Center - Main.screenPosition, null, color, Projectile.rotation, origin, scale, SpriteEffects.None, 0);
+
+			Main.spriteBatch.End();
+			Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
+
+			return false;
+		}
+
+	}
+}
