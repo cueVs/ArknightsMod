4dccde9b65e436db287468e8379bd4462358b90c
cueV <118194388+cueVs@users.noreply.github.com>
2024-05-06 00:24:35 +0900

    Add Indigo's costume

---
+++ Content/Items/Armor/Vanity/Caster/IndigoBody.cs
@@ -0,0 +1,36 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace ArknightsMod.Content.Items.Armor.Vanity.Caster
+{
+	// See also: ExampleCostume
+	[AutoloadEquip(EquipType.Body)]
+	public class IndigoBody : ModItem
+	{
+		public override void SetStaticDefaults() {
+			// DisplayName.SetDefault("Arknights Doctor's Jacket");
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+			if (Main.netMode == NetmodeID.Server)
+				return;
+			ArmorIDs.Body.Sets.HidesTopSkin[Item.bodySlot] = true;
+			ArmorIDs.Body.Sets.HidesArms[Item.bodySlot] = true;
+		}
+
+		public override void SetDefaults() {
+			Item.width = 16;
+			Item.height = 16;
+			Item.rare = ItemRarityID.LightRed; // Same rarity as Arknights
+			Item.vanity = true;
+		}
+
+		//public override void AddRecipes()
+		//{
+		//    Recipe recipe = CreateRecipe();
+		//    recipe.AddRecipeGroup(RecipeGroupID.Wood, 2);
+		//    recipe.AddTile(TileID.WorkBenches);
+		//    recipe.Register();
+		//}
+	}
+}
---
+++ Content/Items/Armor/Vanity/Caster/IndigoHead.cs
@@ -0,0 +1,36 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace ArknightsMod.Content.Items.Armor.Vanity.Caster
+{
+	// See also: ExampleCostume
+	[AutoloadEquip(EquipType.Head)]
+	public class IndigoHead : ModItem
+	{
+		public override void SetStaticDefaults() {
+			// DisplayName.SetDefault("Arknights Doctor's Hood");
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+			if (Main.netMode == NetmodeID.Server)
+				return;
+			ArmorIDs.Head.Sets.DrawHead[Item.headSlot] = false;
+			ArmorIDs.Head.Sets.IsTallHat[Item.headSlot] = true;
+		}
+
+		public override void SetDefaults() {
+			Item.width = 16;
+			Item.height = 16;
+			Item.rare = ItemRarityID.LightRed; // Same rarity as Arknights
+			Item.vanity = true;
+		}
+
+		//public override void AddRecipes()
+		//{
+		//    Recipe recipe = CreateRecipe();
+		//    recipe.AddRecipeGroup(RecipeGroupID.Wood, 2);
+		//    recipe.AddTile(TileID.WorkBenches);
+		//    recipe.Register();
+		//}
+	}
+}
